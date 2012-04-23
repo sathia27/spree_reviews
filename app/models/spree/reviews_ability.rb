2ac76f0088b7f68b915d7ef798db45458bc85dd1
Sathia <sathia2704@gmail.com>
2012-04-23 16:40:43 +0530

    Update app/models/spree/reviews_ability.rb

--- app/models/spree/reviews_ability.rb
+++ app/models/spree/reviews_ability.rb
@@ -3,7 +3,7 @@ class Spree::ReviewsAbility
 
   def initialize(user)
     can :create, Spree::Review do |review, product|
-      (user.has_role?(:user) || !Spree::Reviews::Config[:require_login]) and user.orders.where(:status => "complete").line_items.pluck(:variant_id).include?(Spree::Variant.find_by_product_id(@product.id).id
+      (user.has_role?(:user) || !Spree::Reviews::Config[:require_login])
     end
     can :create, Spree::FeedbackReview do |review|
       user.has_role?(:user) || !Spree::Reviews::Config[:require_login]
